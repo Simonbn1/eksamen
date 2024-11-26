fc99797ab16a20532e73d4ad8cf6eaa89f131fb7
simon <sini022@student.kristiania.no>
2024-11-26 18:49:33 +0100

    updated

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,9 +1,9 @@
-import API_URL from './config';
+import API_URL from "./config";
 
 export const fetchEvents = async () => {
-    const response = await fetch(`${API_URL}/api/event`);
-    if (!response.ok) {
-        throw new Error('Failed to fetch events');
-    }
-    return response.json();
-}
+  const response = await fetch(`${API_URL}/api/event`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch events");
+  }
+  return response.json();
+};
