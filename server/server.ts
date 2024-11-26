a06f55e8155615c663e4a65cdee762f83f009c07
simon <sini022@student.kristiania.no>
2024-11-26 21:23:24 +0100

    updated

--- server/server.ts
+++ server/server.ts
@@ -17,10 +17,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const allowedOrigins = process.env.CLIENT_URL?.split(",") || [];
-app.use(cors({
+app.use(
+  cors({
     origin: allowedOrigins,
     credentials: true,
-}));
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
