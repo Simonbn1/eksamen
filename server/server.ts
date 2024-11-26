92af0faa03914e4e14370cfdc1a25c2d14653ccb
simon <sini022@student.kristiania.no>
2024-11-26 23:25:10 +0100

    updated

--- server/server.ts
+++ server/server.ts
@@ -16,6 +16,7 @@ const ENTRAID_CLIENT_SECRET = process.env.ENTRAID_CLIENT_SECRET;
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
