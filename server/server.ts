5143a70ce91ef42ee40d5310d49ebb843ef971f0
simon <sini022@student.kristiania.no>
2024-11-26 20:31:54 +0100

    updated

--- server/server.ts
+++ server/server.ts
@@ -873,6 +873,29 @@ client.connect().then(() => {
       }
     },
   );
+
+    app.get("/api/user", async (req: Request, res: Response): Promise<void> => {
+        try {
+        const usersCollection = client.db("eventdb").collection("users");
+        const users = await usersCollection.find().toArray();
+        res.status(200).json(users);
+        } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ message: "Internal server error" });
+        }
+    });
+
+    app.get("api/event", async (req: Request, res: Response): Promise<void> => {
+        try {
+        const eventsCollection = client.db("eventdb").collection("eventdb");
+        const events = await eventsCollection.find().toArray();
+        res.status(200).json(events);
+        } catch (error) {
+        console.error("Error fetching events:", error);
+        res.status(500).json({ message: "Internal server error" });
+        }
+    });
+
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.listen(port, "0.0.0.0", () => {
